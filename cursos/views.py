bd2517e6a0965efa51d5f9d0e9f2e3d03f17c36c
Seu Nome <voce@exemplo.com>
2022-07-18 14:06:15 -0300

    Setup inicial

---
+++ cursos/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import reverse, render,get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView
+from django.db.utils import IntegrityError
+
+from .models import Curso, CursoLikes
+from .forms import CursoModelForm
+
+
+def pagina_inicial(request):
+    return render(request, "cursos/pagina_inicial.html")
+
+
+def listar_cursos(request):
+    ordem = request.GET.get("ordenacao", "nome")
+    cursos = Curso.objects.all().order_by(ordem)
+    context = {
+        'cursos': cursos,
+    }
+    return render(request, 'cursos/listar_cursos.html', context)
+
+
+def listar_aulas(request, pk):
+    curso = Curso.objects.get(id=pk)
+    context = {
+        "curso": curso,
+        "aulas": curso.aulas.all()
+    }
+    return render(request, 'cursos/listar_aulas.html', context)
+
+
+class CursoMixin(object):
+    model = Curso
+    form_class = CursoModelForm
+    context_object_name = "curso"
+
+    def get_success_url(self):
+        return reverse("cursos.listar.tudo")
+
+
+class NovoCursoView(CursoMixin, CreateView):
+    template_name = "cursos/curso_novo.html"
+
+
+class AlterarCursoView(CursoMixin, UpdateView):
+    template_name = "cursos/curso_alterar.html"
+
+def like_curso(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    try:
+        CursoLikes.objects.create(user=request.user, curso=curso)
+        context = {
+            'mensagem': f'{curso.autor} agradece!'
+        }
+    except IntegrityError as error:
+        CursoLikes.objects.get(user = request.user, curso=curso).delete()
+        context = {
+            'mensagem': ':('
+        }
+
+    return render(request, "cursos/like_concluido.html", context)
